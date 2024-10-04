5a3a2f754d6f066d76c711c0177fccc0fc704e07
Sagar <deshpandesagar187@gmail.com>
2024-10-04 17:51:50 +0530

    created sign-in route and its ui page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -51,13 +51,29 @@ function SignUp() {
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-        <input type="text" placeholder='username' className='border p-3 rounded-lg focus:outline-none' id='username'
+        <input
+          type="text"
+          placeholder='username'
+          className='border p-3 rounded-lg focus:outline-none'
+          id='username'
           onChange={handleChange} />
-        <input type="email" placeholder='email' className='border p-3 rounded-lg focus:outline-none' id='email'
+
+        <input
+          type="email"
+          placeholder='email'
+          className='border p-3 rounded-lg focus:outline-none'
+          id='email'
           onChange={handleChange} />
-        <input type="password" placeholder='password' className='border p-3 rounded-lg focus:outline-none' id='password'
+
+        <input
+          type="password"
+          placeholder='password'
+          className='border p-3 rounded-lg focus:outline-none'
+          id='password'
           onChange={handleChange} />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase 
+
+        <button
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase 
         hover:opacity-95 disabled:opacity-80 cursor-pointer font-semibold' disabled={loading}>
           {loading ? 'Loading...' : 'Sign Up'}
         </button>
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,8 @@
 const User = require('../models/userModel');
 const bcryptjs = require('bcryptjs');
+const errorHandler = require('../utils/error');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -15,4 +18,26 @@ const signup = async (req, res, next) => {
     }
 };
 
-module.exports = signup;
+const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, "Unauthorize User", "User not found!"));
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(404, "Unauthorize User", "Password was incorrect!"));
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: pass, ...userinfo } = validUser._doc;
+
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(userinfo);
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = {
+    signin,
+    signup,
+};
