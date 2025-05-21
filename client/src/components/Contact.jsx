a21e8211542701487b5b9976ccbf52476c549fb8
Sagar <deshpandesagar187@gmail.com>
2025-05-21 15:01:26 +0530

    Solved problem of sending mail to landlord

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,18 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-
 const Contact = ({ listing }) => {
-
     const [landlord, setLandlord] = useState(null);
     const [message, setMessage] = useState('');
 
     useEffect(() => {
         const fetchLandlord = async () => {
             try {
-
-                const res = await fetch(`/api/user/${listing.userRef}`);
+                const res = await fetch(`/api/user/${listing?.userRef}`);
                 const data = await res.json();
-
                 setLandlord(data);
             } catch (error) {
                 console.log(error.message);
@@ -20,19 +15,24 @@ const Contact = ({ listing }) => {
         };
 
         fetchLandlord();
-    }, [listing.userRef]);
+    }, [listing?.userRef]);
 
     const handleMessage = (e) => {
-
         setMessage(e.target.value);
     };
+
+    const emailBody = encodeURIComponent(message);
+    const mailtoLink = landlord
+        ? `mailto:${landlord?.email}?subject=Regarding ${listing?.name}&body=${emailBody}`
+        : '#';
 
     return (
         <>
             {landlord && (
                 <div className="flex flex-col gap-2">
-                    <p>Contact <span className='font-semibold'>{landlord.username}</span> for
-                        <span className='font-semibold'>{" " + listing.name.toLowerCase()}</span>
+                    <p>
+                        Contact <span className="font-semibold">{landlord?.username}</span> for
+                        <span className="font-semibold">{" " + listing?.name.toLowerCase()}</span>
                     </p>
 
                     <textarea
@@ -40,15 +40,17 @@ const Contact = ({ listing }) => {
                         id="message"
                         rows="2"
                         value={message}
-                        placeholder='Enter your message here'
+                        placeholder="Enter your message here"
                         onChange={handleMessage}
-                        className='w-full border p-3 rounded-lg'></textarea>
+                        className="w-full border p-3 rounded-lg"
+                    ></textarea>
 
-                    <Link
-                        to={`mailto: ${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
-                        className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'>
+                    <a
+                        href={mailtoLink}
+                        className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
+                    >
                         Send Message
-                    </Link>
+                    </a>
                 </div>
             )}
         </>
